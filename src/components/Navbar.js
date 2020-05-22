35a46498298f68ac59ed302a7c3cef667a8f95cd
marija-marinkovicm <marija@tourradar.com>
2020-05-22 14:28:04 +0200

    branding update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <img src={logo} alt="Kult" style={{ width: '88px' }} />
             </Link>
             {/* Hamburger menu */}
             <div
@@ -61,32 +61,17 @@ const Navbar = class extends React.Component {
           >
             <div className="navbar-start has-text-centered">
               <Link className="navbar-item" to="/about">
-                About
+                Dom
               </Link>
               <Link className="navbar-item" to="/products">
-                Products
+                Putovanja
               </Link>
               <Link className="navbar-item" to="/blog">
-                Blog
-              </Link>
-              <Link className="navbar-item" to="/contact">
-                Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
+                Umetnost
               </Link>
             </div>
             <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
+              <p className="handwrite navbar-item">Magazin za kulturu življenja</p>
             </div>
           </div>
         </div>
